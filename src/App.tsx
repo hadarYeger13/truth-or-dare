90e3afa24859bf32b44d1a07e475281bb18d7023
hadarYeger <hadar.yeger@sundaysky.com>
2025-08-08 16:00:30 +0300

    Initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,76 @@
-import { useState } from 'react'
-import './App.css'
+import { Box, SxProps, ButtonBase, Dialog } from '@mui/material'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const dialogType = useRef<"truth" | "dare" | null>(null);
 
-  return (
-    <>
-      <div>
-        <h1>Truth or Dare</h1>
-        <p>Welcome to the Truth or Dare game!</p>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const onTruthClick = () => {
+    setIsDialogOpen(true);
+    dialogType.current = "truth";
+  }
+
+  const onDareClick = () => {
+    setIsDialogOpen(true);
+    dialogType.current = "dare";
+  }
+
+  const onDialogClose = () => {
+    setIsDialogOpen(false);
+    dialogType.current = null;
+  }
+
+  return <>
+    <Box sx={containerSxProps}>
+      <ButtonBase sx={truthCardSxProps}
+        onClick={onTruthClick}
+      >
+        Truth
+      </ButtonBase>
+      <ButtonBase sx={dareCardSxProps}
+        onClick={onDareClick}
+      >
+        Dare
+      </ButtonBase>
+    </Box>
+    <Dialog
+        open={isDialogOpen}
+        onClose={onDialogClose}
+    >
+        Dialog content
+    </Dialog>
+  </>
+}
+
+const containerSxProps: SxProps ={
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "24px"
+}
+
+const cardSxProps: SxProps = {
+  width: "120px",
+  height: "240px",
+  borderRadius: "8px",
+  borderWidth: "5px",
+  borderStyle: "solid",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+};
+
+const truthCardSxProps: SxProps = {
+  ...cardSxProps,
+  backgroundColor: "pink",
+  borderColor: "deeppink"
+};
+
+const dareCardSxProps: SxProps = {
+  ...cardSxProps,
+  backgroundColor: "cornflowerblue",
+  borderColor: "blue"
 }
 
 export default App
---
+++ src/Card.tsx
@@ -0,0 +1,12 @@
+import { Box, SxProps } from '@mui/material'
+
+export const Card = () => {
+    return <Box sx={containerSxProps}>
+
+    </Box>
+}
+
+const containerSxProps: SxProps = {
+    width: "120px",
+    height: "160px"
+}
